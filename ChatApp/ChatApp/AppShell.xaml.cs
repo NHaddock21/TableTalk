1fabab36162c3582fcd0c7f9af8f29488c8a6529
Nikolas Haddock <nikolas.haddock@snhu.edu>
2023-01-24 17:06:26 -0500

    Add project files.

---
+++ ChatApp/ChatApp/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ChatApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
